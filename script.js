8fd0f45bb66b2dc71289856ba528b4c38f7eb149
isaac <ifhumail@gmail.com>
2026-07-15 06:31:21 +0200

    Update script.js - organize game logic

---
+++ script.js
@@ -0,0 +1,38 @@
+let gameState = {
+  unlocked: [],
+  currencies: {},
+  combinations: {},
+  discoveryOrder: [],
+};
+
+// Colors Math
+function mixHue(h1, h2) {}
+function mixColors(c1, c2) {}
+
+function hslToRgb(h, s, l) {}
+
+// Find Logic
+
+function nearestColorName(rgb) {}
+
+// Drag & Drop
+function onDragStart(e) {}
+function onDrop(e) {}
+
+// Render
+
+function render() {}
+
+// Game tick
+
+setInterval(() => {}, 1000);
+
+// Save Logic
+
+function save() {}
+
+function load() {}
+
+// Init
+load();
+render();
